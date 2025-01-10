1f785c173c2a1345d7bac12f39f34b58e13e3260
Baran Özcan <melih.dilbaz@sabanciuniv.edu>
2025-01-10 17:53:07 +0300

    small tweaks

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,11 @@ export function Navbar() {
     return `
       <nav class="bg-white shadow">
         <div class="container mx-auto px-4 py-4 flex justify-between items-center">
-          <a href="#" class="text-2xl font-bold text-blue-600">DataSciencePro</a>
+          <a href="#" class="text-2xl font-bold text-blue-600">Melih Dilbaz Project</a>
           <div>
             <a href="#introduction" class="text-gray-600 hover:text-blue-600 mx-2">Introduction</a>
-            <a href="#hypothesis" class="text-gray-600 hover:text-blue-600 mx-2">Hypothesis Testing</a>
-            <a href="#graphs" class="text-gray-600 hover:text-blue-600 mx-2">Interesting Graphs</a>
-            <a href="#network" class="text-gray-600 hover:text-blue-600 mx-2">Network Graphs</a>
+            <a href="#hypotheses" class="text-gray-600 hover:text-blue-600 mx-2">Hypothesis Testing</a>
+            <a href="#eda" class="text-gray-600 hover:text-blue-600 mx-2">EDA</a>
             <a href="#machine-learning" class="text-gray-600 hover:text-blue-600 mx-2">Machine Learning</a>
             <a href="#conclusion" class="text-gray-600 hover:text-blue-600 mx-2">Conclusion</a>
           </div>
